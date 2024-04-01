e3dab9e8c31b905ec1fb314800f90f8b0ea6b0cf
Daniel Nimsgern <nimsgernd@msoe.edu>
2024-03-31 20:31:41 -0500

    Changed comments and cleaned up. At the point where I need to start working with hardware but I'm too broke for that.

--- arduino/deej-custom/deej-custom.ino
+++ arduino/deej-custom/deej-custom.ino
@@ -21,19 +21,26 @@
 /******************************************************************************
  * Variables
  *****************************************************************************/
+
 /* Analog Inputs */
 #define NUM_POTS      (int) 12
-static const int analogPotInputs[NUM_POTS] = {A0, A1, A2, A3, A4, A5, A6, A7,
-                                              A8, A9, A10, A11};
   /*****************
    | Master : A0
    | Mic    : A1
    | Faders : A2-A11
    *****************/
+static const int analogPotInputs[NUM_POTS] = {A0, A1, A2, A3, A4, A5, A6, A7,
+                                              A8, A9, A10, A11};
 
 /* Digital Inputs */
 #define NUM_MUTE_BUTTONS  (int) 12
 #define NUM_MACRO_BUTTONS (int) 16
+  /**************************
+   | Master Mute : 68
+   | Mic Mute    : 69
+   | Fader Mutes : 5-13, 20
+   | Macro 1-16  : 22-52 even
+   **************************/
 static const int digitalMuteButtonInputs[NUM_MUTE_BUTTONS] = {68, 69, 5, 6, 7,
                                                               8, 9, 10, 11, 12,
                                                               13, 20};
@@ -42,34 +49,30 @@ static const int digitalMacroButtonInputs[NUM_MACRO_BUTTONS] = {22, 24, 26, 28,
                                                                 38, 40, 42, 44,
                                                                 46, 48, 50, 52
                                                                 };
-  /**************************
-   | Master Mute : 68
-   | Mic Mute    : 69
-   | Fader Mutes : 5-13, 20
-   | Macro 1-16  : 22-52 even
-   **************************/
 
 /* LED Pins */
 #define NUM_LEDS      (int) 12
-static const int digitalLEDOutputs[NUM_LEDS] = {14, 15, 23, 25, 27, 29, 31, 33,
-                                                35, 37, 39, 41};
   /********************
    | Master : 14
    | Mic    : 15
    | Faders : 23-41 odd
    ********************/
+static const int digitalLEDOutputs[NUM_LEDS] = {14, 15, 23, 25, 27, 29, 31, 33,
+                                                35, 37, 39, 41};
 
 /* OLED Values */
 #define SCREEN_WIDTH  (int) 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT (int) 64 // OLED display height, in pixels
 
+#define NUM_DISPLAYS  (int) 3
+  /******************
+   | OLED Reset : 0
+   | OLED DC    : 1
+   | OLED CS    : 2-4
+   ******************/
 #define OLED_RESET    (int) 0
 #define OLED_DC       (int) 1
-#define NUM_DISPLAYS  (int) 3
 static const int oledCS[NUM_DISPLAYS] = {2, 3, 4};
-  /***************
-   | OLED CS : 2-4
-   ***************/
 
 /* Program Data */
 static int analogPotValues[NUM_POTS];
@@ -103,13 +106,15 @@ void setup() {
   for (int i = 0; i < NUM_MUTE_BUTTONS; i++)
   {
     pinMode(digitalMuteButtonInputs[i], INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(digitalMuteButtonInputs[i]), muteButtonPress, FALLING);
+    attachInterrupt(digitalPinToInterrupt(digitalMuteButtonInputs[i]),
+                    muteButtonPress, FALLING);
   }
 
   for (int i = 0; i < NUM_MACRO_BUTTONS; i++)
   {
     pinMode(digitalMacroButtonInputs[i], INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(digitalMuteButtonInputs[i]), macroButtonPress, FALLING);
+    attachInterrupt(digitalPinToInterrupt(digitalMuteButtonInputs[i]),
+                    macroButtonPress, FALLING);
   }
 
   /* LED Outputs */
